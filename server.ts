c1736463d711f3501eb37333c546357d65f950d7
nhq1993-sudo <nhq1993@gmail.com>
2026-03-05 13:31:49 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -49,13 +49,14 @@ async function startServer() {
       }
 
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
-
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
       const mailOptions = {
         from: `"AdCMS PRO" <${process.env.EMAIL_USER}>`,
         to: recipientEmail,
